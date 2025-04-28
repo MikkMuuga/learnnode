c900e90d46d29d7952d95251d91d48bec3fd8abf
Mikk <gregormikk@gmail.com>
2025-04-28 15:34:25 +0300

    Add axios

--- src/pages/Chuck.vue
+++ src/pages/Chuck.vue
@@ -1,12 +1,21 @@
 <script setup>
 
 import {ref} from 'vue';
+import axios from 'axios';
+
 let joke = ref('');
 
-let response = await fetch('https://api.chucknorris.io/jokes/random');
-let data = await response.json();
-console.log(data);
-joke.value = data.value;
+//axios.get('https://api.chucknorris.io/jokes/random').then(response => {
+//    joke.value = response.data.value;
+//}).catch(error => {
+//    console.error('Error fetching joke:', error);
+//});
+try {
+let response = await axios.get('https://api.chucknorris.io/jokes/random');
+joke.value = response.data.value;
+} catch (error) {
+    console.error('Error fetching joke:', error);
+}
 </script>
 
 <template>

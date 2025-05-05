426dce343fcd132bf12207bcf730260881c73202
Mikk <gregormikk@gmail.com>
2025-05-05 18:37:47 +0300

    Add websocket example

--- bin/server.js
+++ bin/server.js
@@ -1,63 +1,23 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import {WebSocketServer} from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
 
 let messages = [];
 
-app.use(express.json());
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+        data = JSON.parse (data.toString());
+        messages.push(data);
+        wss.clients.forEach(client => {
+            if(client.readyState === client.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+      });
+
+ws.send(JSON.stringify({type: 'messages', messages}));
 });
 
-app.get('/', (req, res) => {
-    let date = req.query.date ?? null;
-    let filteredMessages = messages.filter(message => message.date > new Date(date));
-    res.json(filteredMessages);
-});
-
-app.get('/longpoll', async (req, res) => {
-    let date = req.query.date ?? null;
-    let filteredMessages;
-    do {
-        await SingleEntryPlugin(1000);
-        filteredMessages = messages.filter(message => message.date > new Date(date));
-        } while (filteredMessages.length === 0);
-    res.json(filteredMessages);
-});
-
-app.get('/sse', async (req, res) => {
-    res.Header('Content-Type', 'text/event-stream');
-    res.Header('Cache-Control', 'no-cache');
-    res.Header('Connection', 'keep-alive');
-    let closed = false;
-    req.on('close', () => {
-        closed = true;
-    });	
-
-    let date = new date();
-    let filteredMessages;
-    do {
-        await sleep(1000);
-        filteredMessages = messages.filter(message => message.date > new Date(date));
-        while (filteredMessages.length === 0){
-            let LastMessage = filteredMessages[filteredMessages.length - 1];
-            date = LastMessage.date;
-        }
-        res.write(`event: message\n`);
-        res.write(`data: ${JSON.stringify(filteredMessages)}\n\n`);
-    }while (!closed)
-    res.end();
-});
-
-
-app.post('/', (req, res) => {
-    messages.push({message: req.body.message, date: new Date()});
-    res.json(req.body);
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -5,22 +5,27 @@ import { ref } from 'vue';
 let messages = ref([]);
 let input = ref('');
 
-let res = await axios.get('https://localhost:3000');
-messages.value = res.data;
+// Create WebSocket connection.
+const socket = new WebSocket("ws://localhost:8080");
 
-const eventSource = new EventSource('https://localhost:3000/sse');
+// Connection opened
+socket.addEventListener("open", (event) => {
+  socket.send("Hello Server!");
+});
 
-eventSorce.addEventListener('newmessage', (event) => {
-    console.log(event);
-    let data = JSON.parse(event.data);
-    messages.value.push(...data);
+// Listen for messages
+socket.addEventListener("message", (event) => {
+  console.log("Message from server ", event.data);
+  let data = JSON.parse(event.data);
+  if (data.type === 'message') {
+    messages.value = data.messages;
+  } else if (data.type === 'jou') {
+    messages.value.push(data);
+  }
 });
 
 async function send() {
-    await axios.post('https://localhost:3000', {
-        message: input.value
-    });
-    console.log(res);
+    socket.send(JSON.stringify({ type: 'message', message: input.value }));
     input.value = '';
 }
 </script>
